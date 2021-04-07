df6ea6da6f9f1fc8e3b21259af48a4f6639bf502
malesani <alejandroMalesani@hotmail.com>
2021-04-07 16:35:00 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/* dati inseriti */
+var km = prompt ("dimmi quanti km vuoi fare");
+var età = prompt ("quanti anni hai?");
+
+console.log(km);
+console.log(età);
+
+/* regole calcolo*/
+var costo = km * 0.21;
+var sconto_G = costo ;
+var sconto_P = costo ;
+
+console.log(costo);
+console.log(sconto_G);
+console.log(sconto_P);
+
+/* resultato */
+
+if(età </ 18){
+  /* istruzione  */
+}else if (età >/65){
+  /* istruzione */
+}else {
+
+}
+
+
+
+
+
+
